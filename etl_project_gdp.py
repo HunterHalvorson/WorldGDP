399befa20f3766ed8a4872140b31281cd6789108
HunterHalvorson <126722998+HunterHalvorson@users.noreply.github.com>
2025-07-27 15:02:52 -0600

    .

---
+++ etl_project_gdp.py
@@ -0,0 +1,113 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sqlite3
+import numpy as np
+from datetime import datetime
+
+
+URL = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
+
+# Code for ETL operations on Country-GDP data
+
+
+
+def extract(url, table_attribs):
+    
+    # Extract the web page as text
+    r = requests.get(url)
+    pageText = r.text
+    
+    # Parse the text into an HTML object
+    soup = BeautifulSoup(pageText, 'html.parser')
+
+    # Create an empty pandas DataFrame named df with columns as the table_attribs
+    df = pd.DataFrame(columns = table_attribs)
+    
+    # Extract all 'tbody' attributes of the HTML object and then extract all the rows of the index 2 table using the 'tr' attribute.
+    table = soup.find_all('tbody')
+    table_world = table[2]
+    table_world_rows = table_world.find_all('tr')
+
+    for row in table_world_rows:
+        cols = row.find_all('td')
+
+        if not cols:
+            continue
+        if not cols[0].find('a'):
+            continue
+        if cols[2].text.strip() == '—':
+            continue
+        
+        name = cols[0].a.text
+        population = cols[2].text
+
+        data_dict = {"Country": name, "GDP_USD_million": population}
+        df1 = pd.DataFrame(data_dict, index = [0])
+        df = pd.concat([df,df1], ignore_index=True)
+      
+    return df
+
+
+def transform(df):
+    ''' This function converts the GDP information from Currency
+    format to float value, transforms the information of GDP from
+    USD (Millions) to USD (Billions) rounding to 2 decimal places.
+    The function returns the transformed dataframe.'''
+    GDP_LIST = df['GDP_USD_million'].tolist()
+    GDP_LIST = [float("".join(x.split(','))) for x in GDP_LIST]
+    GDP_LIST = [round(x/1000, 2) for x in GDP_LIST]
+    df['GDP_USD_millions'] = GDP_LIST
+    df = df.rename(columns = {'GDP_USD_millions': "GDP_USD_billions"})
+    print(df)
+    return df
+
+def load_to_csv(df, csv_path):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+    df.to_csv(csv_path)
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final dataframe as a database table
+    with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists="replace", index = False)
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+def log_progress(message):
+    ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
+    timestamp_format = '%Y-%h-%d-%H-%M-%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')    
+
+
+''' Here, you define the required entities and call the relevant 
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+log_progress('Preliminaries complete. Initiating ETL process')
+df = extract(URL, table_attribs)
+log_progress('Data extraction complete. Initiating Transformation process')
+df = transform(df)
+log_progress('Data transformation complete. Initiating loading process')
+load_to_csv(df, csv_path)
+log_progress('Data saved to CSV file')
+sql_connection = sqlite3.connect('World_Economies.db')
+log_progress('SQL Connection initiated.')
+load_to_db(df, sql_connection, table_name)
+log_progress('Data loaded to Database as table. Running the query')
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+log_progress('Process Complete.')
+sql_connection.close()
+
